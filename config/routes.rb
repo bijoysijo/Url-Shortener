4070ad2f92a4eadb739c55078213b6c9b0debe1b
Bijoy Sijo <bijoysijo21@gmail.com>
2020-08-23 17:50:26 +0530

    add url resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'urls/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :url, only: [:index, :new, :create, :show]
 end
