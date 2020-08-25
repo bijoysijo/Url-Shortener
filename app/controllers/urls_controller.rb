9deb331f4b4736363c8d2a29e0d6f85ce95df151
Bijoy Sijo <bijoysijo21@gmail.com>
2020-08-25 23:29:30 +0530

    urls controller and index view

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,4 +1,30 @@
 class UrlsController < ApplicationController
   def index
   end
+
+  def create
+    @input_url = Url.new(url_params)
+    @input_url.shortened_url = @input_url.short_url
+
+    if @input_url.save
+      redirect_to url_path(@input_url)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @find = Url.find(params[:id])
+  end
+
+  def redirect
+    @url = Url.find_by(shortened_url: params[:shortened_url])
+    redirect_to @url.user_url
+  end
+
+  private
+
+  def url_params
+    params.permit(:user_url)
+  end
 end
