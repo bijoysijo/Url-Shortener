5e1844fedbbcb2a34a7340affbb0b9e1e1819146
Bijoy Sijo <bijoysijo21@gmail.com>
2020-08-25 23:31:53 +0530

    custom route for shortened url

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  resources :url, only: [:index, :new, :create, :show]
+  resources :urls, only: [:index, :new, :create, :show]
+  root 'urls#index'
+  get ':shortened_url', to: 'urls#redirect'
+  # resources :brands, only: [:index, :show] do
+  #   resources :products, only: [:index, :show]
+  # end
+  # resources :photos
+  # resource :photo
+  get 'photos/:id', to: 'photos#show', defaults: { format: 'jpg' }
+
+  # resource :basket, only: [:show, :update, :destroy]
+
+  # resolve("Basket") { route_for(:basket) }
 end
